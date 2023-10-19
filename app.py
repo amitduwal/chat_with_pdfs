7b0f2ae0879ee44cbdf3f1940e78a351c888864a
amitduwal <duwalamit@gmail.com>
2023-10-19 11:44:23 +0545

    use alpaca

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def get_vectorstore(text_chunks):
 
 def  get_conversation_chain(vectorstore):
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-    # llm = HuggingFaceHub(repo_id="declare-lab/flan-alpaca-large", model_kwargs={"temperature":0.5, "max_length":1024})
-    llm = HuggingFaceHub(repo_id="eachadea/vicuna-7b-1.1", model_kwargs={"temperature":0, "max_length":512})
+    llm = HuggingFaceHub(repo_id="declare-lab/flan-alpaca-large", model_kwargs={"temperature":0.5, "max_length":1024})
+    # llm = HuggingFaceHub(repo_id="eachadea/vicuna-7b-1.1", model_kwargs={"temperature":0, "max_length":512})
 
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages = True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
